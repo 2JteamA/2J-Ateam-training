e92aab1089238603f5b32206b4df85fe90b8bcfe
ningyangit <neigan2007@gmail.com>
2025-07-06 23:15:41 +0900

    CICD設定、一部ソース修正

--- stringboot-app/src/main/java/com/example/stringboot_app/config/WebConfig.java
+++ stringboot-app/src/main/java/com/example/stringboot_app/config/WebConfig.java
@@ -4,20 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${custom.server.ip}")
+    private String serverIp;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-               
-                registry.addMapping("/**")
-                         .allowedOrigins("http://localhost:4200") 
+
+                registry.addMapping("/api/**")
+                         .allowedOriginPatterns(serverIp)    // ローカル用、サーバ用が異なるため、プロパティファイルから取得
                          .allowedMethods("PUT", "POST", "GET", "DELETE")
-                         .allowedHeaders("*");
+                         .allowedHeaders("*")
+                         .allowCredentials(true);
             }
         };
     }
